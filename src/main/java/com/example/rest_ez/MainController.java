c14c5d5fee25e3072c6cf329e4020522fda35fb2
Benny <bennyhuang1996@outlook.com>
2025-05-19 11:14:27 +0200

    Initial commit

---
+++ src/main/java/com/example/rest_ez/MainController.java
@@ -0,0 +1,16 @@
+package com.example.rest_ez;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    // http://localhost:8080/hello
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
